80a9a7838453da7fb241cb3b210efed69577d6fc
nimie <h267734@stud.u-szeged.hu>
2024-04-21 21:49:46 +0200

    Elemek kicserélése működik

---
+++ megvalositando.md
@@ -0,0 +1,14 @@
+1. A játékmező létrehozását úgy valósítjuk meg, hogy megadjuk a sorok és oszlopok számát, majd ezek alapján méretezzük és elrendezzük az elemeket. Például, ha 8 sor és oszlop van, és az elemek mérete 64px széles, akkor kiszámoljuk az egyenletes térközt az elemek között. Ennek lehet egy MIN és egy MAX értéke is, amelyeket beállíthatunk, és amelyek határozzák meg a tábla és az elemek közötti helyet.
+2. Készítünk egy metódust az elemek létrehozására a táblában. Ez úgy történik, hogy minden elemnek egyedi azonosítót adunk, vagy egyedileg hivatkozhatunk rájuk, így ha a felhasználó rákattint egy elemre, tudjuk, melyikre kattintott, és bármelyik másik elemet is megtalálhatjuk.
+3. Implementálunk egy módszert, amellyel a felhasználó képes lesz felcserélni két elemet. Ehhez létrehozunk néhány mérőszámot, amelyek segítenek eldönteni, hogy az elemek cserélhetőek-e, mennyire közel vannak egymáshoz, stb.
+4. Ha a felhasználó felcseréli az elemeket, és 3 vagy több ugyanolyan típusú elem van egy sorban vagy oszlopban, ellenőrizzük, hogy van-e további csatlakozó elem, amelyből további elemek eltüntethetőek. Az összes releváns elemet megjelöljük, majd eltávolítjuk őket, és animációval eltüntetjük.
+5. Az eltűnő elemek után megnézzük, hogy van-e elem az eltávolított elemek fölött. Ha van, ezek az elemek kapnak egy gravitációs osztályt, és lefele esnek.
+6. Ha volt eltűnő elem, új elemeket hozunk létre fentről, amelyek szintén kapnak egy gravitációs osztályt, majd lefuttatjuk a leesést. Ha üres hely marad a pályán, új elemeket hozunk létre, amíg be nem telik a tábla.
+7. Az eltűnő elemek alapján kiszámítjuk, hogy mennyi pontot érnek. Minden eltűnt elem 10 pontot ér, és minden további eltűnt ugyanolyan színű elemért a pontszámot megszorozzuk 1.5-tel. Ezután összegezzük a pontokat az eltűnt elemek számával.
+8. Hozzáadjuk a megszerzett pontokat a pontszámlálóhoz, és ha elérünk egy bizonyos pontszámot, átlépünk a következő pályára.
+9. Az első cserénél elindítjuk a stoppert, és folyamatosan csökkentjük. Ha a felhasználó elemeket cserél, és van eltűnt elem, az időt hozzáadhatjuk.
+10. Amikor az elemekkel interakcióba lépünk, animációt és hanghatást játszunk le.
+11. Ha lejár az idő vagy a felhasználó rákattint a feladás gombra, fel kell adnia egy felhasználónevet és az adott pontszámot elmentjük. Ezeket a list.html oldalon lehet megtekinteni.
+12. A játék alatt rúnák is eshetnek alacsony eséllyel, amelyek különböző bónuszokat nyújthatnak. Ezek csak az adott pályán érvényesek, de bármennyi ideig tárolhatjuk őket.
+//TODO: Kitalálni legalább 5 power up-ot.
+//TODO: A következő pályákhoz ki kell találnunk valami eltérést az elsőhöz képest.
--- script.js
+++ script.js
@@ -1,78 +1,162 @@
-$(document).ready(function() {
-    let timer;
-    loadLeaderboard();
-    const boardSize = 8;
-    const gemCount = boardSize * boardSize;
+$( document ). ready ( function () {
+    const paddingsize = 10;
     const imagePath = 'resources/images/';
     const gameBoard = $('#game-area');
+    const rowSize = 8;
+    const colSize = 8;
+    const gemSize = 64;
+
+    let widthnumber = (rowSize*gemSize)+((rowSize-1)*paddingsize/2);
+    let heightnumber = (colSize*gemSize)+((colSize-1)*paddingsize/2);
+
+    let gameBoardWidth = widthnumber+"px";
+    let gameBoardHeight = heightnumber+"px";
+    let gemTypesBoard = [];
+
+    for(let i = 0; i < rowSize; i++){
+        gemTypesBoard[i] = [];
+        for(let j = 0; j < colSize; j++){
+            gemTypesBoard[i][j] = -1;
+        }
+    }
+
+    $(gameBoard).css({
+        "width": gameBoardWidth,
+        "height": gameBoardHeight,
+        "border": "1px solid black",
+        "padding": paddingsize,
+    });
+
+    let gemArray = [];
 
     function generateGems() {
-        for (let i = 0; i < gemCount; i++) {
-            const gem = $('<img alt="">').attr('src', getRandomGemImagePath()).addClass('gem');
-            gameBoard.append(gem);
+        for (let i = 0; i < rowSize; i++) {
+            gemArray[i] = []; // Minden sorhoz létrehozunk egy üres tömböt
+            for (let j = 0; j < colSize; j++) {
+                let gemSrc = getRandomGemImagePath();
+                let gemType = gemSrc.substring(gemSrc.lastIndexOf('/') + 1, gemSrc.lastIndexOf('.'));
+                const rowIndex = i;
+                const colIndex = j;
+                let gem = $('<img alt="" src="">').attr('src', gemSrc).addClass('gem').addClass(gemType).addClass('_' + rowIndex + '_' + colIndex);
+
+                gemArray[i][j] = gem;
+
+                gem.css({
+                    "position": "absolute",
+                    "width": gemSize,
+                    "height": gemSize,
+                    "margin": paddingsize,
+                    "cursor": "pointer",
+                });
+
+            }
+        }
+    }
+
+    function display() {
+        for (let i = 0; i < rowSize; i++) {
+            for (let j = 0; j < colSize; j++) {
+                let gem = gemArray[i][j];
+                gem.css({
+                    "top": (i * (gemSize + paddingsize / 2)) + ($(gameBoard).get(0).offsetTop)  + ((heightnumber-((gemSize+paddingsize/2)*rowSize)))+paddingsize/2+0.5+ "px",
+                    "left": (j * (gemSize + paddingsize / 2)) + ($(gameBoard).get(0).offsetLeft)  + ((widthnumber-((gemSize+paddingsize/2)*rowSize)))+paddingsize/2+0.5 + "px",
+                });
+                gameBoard.append(gem);
+            }
         }
     }
 
     function getRandomGemImagePath() {
-        const gemTypes = ['gem1.png', 'gem2.png', 'gem3.png', 'gem4.png', 'gem5.png', 'gem6.png'];
+        const gemTypes = ['gem1.png', 'gem2.png', 'gem3.png', 'gem4.png', 'gem5.png', 'gem6.png', 'gem7.png', 'gem8.png'];
         const randomIndex = Math.floor(Math.random() * gemTypes.length);
         return imagePath + gemTypes[randomIndex];
     }
 
-    function initializeGame() {
-        generateGems();
+    function clear() {
+        $(gameBoard).empty(); // gameBoard tartalmának törlése
     }
+
+    function initializeGame() {
+        clear();
+        generateGems();
+        display();
+        getGemstoneTypeArray();
+        printGemstoneTypeArray();
+    }
+
 
     initializeGame();
 
-    function addToLeaderboard(data) {
-        var leaderboardData = localStorage.getItem('leaderboardData');
-        if (!leaderboardData) {
-            localStorage.setItem('leaderboardData', JSON.stringify([data]));
-        } else {
-            var parsedData = JSON.parse(leaderboardData);
-            parsedData.push(data);
-            localStorage.setItem('leaderboardData', JSON.stringify(parsedData));
-        }
 
-        var path = 'leaderboard/points.txt';
-
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-            var fileData = 'Felhasználónév,Pontszám,Dátum\n' + data.join(',') + '\n';
-            var file = new Blob([fileData], {type: 'text/plain'});
-
-            var a = $('<a>').attr('href', URL.createObjectURL(file)).attr('download', path).appendTo('body');
-            a[0].click();
-            window.URL.revokeObjectURL(url);
-            a.remove();
-        } else {
-            console.log('A böngészője nem támogatja a File API-t.');
-        }
-    }
-
-    function loadLeaderboard() {
-        if (window.location.pathname.includes('list.html')) {
-            let leaderboardData = localStorage.getItem('leaderboardData');
-            if (!leaderboardData) {
-                console.log('Nincsenek adatok a ponttáblában.');
-                return;
-            }
-            displayLeaderboard(JSON.parse(leaderboardData));
-        }
-    }
-
-    function displayLeaderboard(data) {
-        let tableBody = $('tbody');
-        tableBody.empty();
-        data.forEach(function(rowData) {
-            let tr = $('<tr>');
-            rowData.forEach(function(cellData) {
-                let td = $('<td>').text(cellData);
-                tr.append(td);
-            });
-            tableBody.append(tr);
+    function getGemstoneTypeArray(){
+        $('.gem').each(function(index) {
+            const rowIndex = Math.floor(index / rowSize);
+            const colIndex = index % rowSize;
+             gemTypesBoard[rowIndex][colIndex] = $(this).attr('class').split(' ')[1][3]; // TÍPUS
         });
+        // gemTypesBoard[rowIndex][colIndex] = $(this).attr('class').split(' ')[2].split('_')[1]// SOR
+        // gemTypesBoard[rowIndex][colIndex] = $(this).attr('class').split(' ')[2].split('_')[2] // OSZLOP
     }
+
+    function printGemstoneTypeArray(){
+        for(let i = 0; i < rowSize; i++){
+            let row = ""
+            for(let j = 0; j < colSize; j++){
+                row = row + (gemTypesBoard[i][j]+ " ")
+            }
+            console.log(row +" \n");
+        }
+    }
+
+    let selectedGem = null;
+
+    $('.gem').on('click', function() {
+        // Ellenőrizzük, hogy van-e már kiválasztott gyémánt
+        if (selectedGem === null) {
+            // Ha nincs kiválasztott gyémánt, akkor jelöljük meg a kattintottat
+            selectedGem = $(this);
+        } else {
+            // Ellenőrizzük, hogy a kattintott gyémánt szomszédos-e a kiválasztottal
+            const selectedRowIndex = parseInt(selectedGem.attr('class').split(' ')[2].split('_')[1]);
+            const selectedColIndex = parseInt(selectedGem.attr('class').split(' ')[2].split('_')[2]);
+            const clickedRowIndex = parseInt($(this).attr('class').split(' ')[2].split('_')[1]);
+            const clickedColIndex = parseInt($(this).attr('class').split(' ')[2].split('_')[2]);
+            console.log("CSERE ELŐTT");
+            console.log("első elem "+selectedGem.attr('class').split(' ')[2]);
+            console.log("második elem "+$(this).attr('class').split(' ')[2]);
+            if ((Math.abs(selectedRowIndex - clickedRowIndex) === 1 && selectedColIndex === clickedColIndex) ||
+                (Math.abs(selectedColIndex - clickedColIndex) === 1 && selectedRowIndex === clickedRowIndex)) {
+                // Ha a mezők szomszédosak, akkor cseréld ki őket egymással
+
+                let temp =  gemArray[selectedRowIndex][selectedColIndex];
+                gemArray[selectedRowIndex][selectedColIndex] = gemArray[clickedRowIndex][clickedColIndex];
+                gemArray[clickedRowIndex][clickedColIndex] = temp;
+
+                gemArray[selectedRowIndex][selectedColIndex].removeClass('_' + selectedRowIndex + '_' + selectedColIndex).addClass('_' + clickedRowIndex + '_' + clickedColIndex);
+                gemArray[clickedRowIndex][clickedColIndex].removeClass('_' + clickedRowIndex + '_' + clickedColIndex).addClass('_' + selectedRowIndex + '_' + selectedColIndex);
+                selectedGem.removeClass('_' + selectedRowIndex + '_' + selectedColIndex).addClass('_' + clickedRowIndex + '_' + clickedColIndex);
+                $(this).removeClass('_' + clickedRowIndex + '_' + clickedColIndex).addClass('_' + selectedRowIndex + '_' + selectedColIndex);
+                display();
+                console.log("Kicserélve!");
+                console.log("CSERE UTÁN");
+                console.log("első elem "+selectedGem.attr('class').split(' ')[2]);
+                console.log("második elem "+$(this).attr('class').split(' ')[2]);
+
+                selectedGem = null;
+            } else {
+                // Ha a mezők nem szomszédosak, akkor a kiválasztott gyémántot null-ra állítjuk vissza
+                selectedGem = null;
+            }
+        }
+    });
+
+    $(window).on('resize', function() {
+        clear();
+        display();
+    });
+
+
+    // JÁTÉK VÉGE:
 
     function gameEnd(score) {
         let username = prompt("Gratulálunk! Pontszámod: " + score + "\nKérlek, add meg a felhasználóneved:");
@@ -81,6 +165,10 @@ $(document).ready(function() {
             addToLeaderboard(data);
         }
     }
+
+
+    // AZ IDŐZÍTÉSHEZ ÉS AZ IDŐSVÁHOZ TARTOZÓ METÓDUSOK:
+    let timer;
 
     function startTimer() {
         let timeLeft = 100;
@@ -128,4 +216,66 @@ $(document).ready(function() {
     function padZero(number) {
         return (number < 10 ? '0' : '') + number;
     }
+
+
+
+
+
+
+
+    // A LEJÁTSZÓTT JÁTÉKOK TÁBLÁHOZ TARTOZÓ METÓDUSOK:
+
+    loadLeaderboard();
+    function addToLeaderboard(data) {
+        let leaderboardData = localStorage.getItem('leaderboardData');
+        if (!leaderboardData) {
+            localStorage.setItem('leaderboardData', JSON.stringify([data]));
+        } else {
+            let parsedData = JSON.parse(leaderboardData);
+            parsedData.push(data);
+            localStorage.setItem('leaderboardData', JSON.stringify(parsedData));
+        }
+
+        let path = 'leaderboard/points.txt';
+
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            let fileData = 'Felhasználónév,Pontszám,Dátum\n' + data.join(',') + '\n';
+            let file = new Blob([fileData], {type: 'text/plain'});
+
+            let a = $('<a>').attr('href', URL.createObjectURL(file)).attr('download', path).appendTo('body');
+            a[0].click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+        } else {
+            console.log('A böngészője nem támogatja a File API-t.');
+        }
+    }
+
+    function loadLeaderboard() {
+        if (window.location.pathname.includes('list.html')) {
+            let leaderboardData = localStorage.getItem('leaderboardData');
+            if (!leaderboardData) {
+                console.log('Nincsenek adatok a ponttáblában.');
+                return;
+            }
+            displayLeaderboard(JSON.parse(leaderboardData));
+        }
+    }
+
+    function displayLeaderboard(data) {
+        let tableBody = $('tbody');
+        tableBody.empty();
+        data.forEach(function(rowData) {
+            let tr = $('<tr>');
+            rowData.forEach(function(cellData) {
+                let td = $('<td>').text(cellData);
+                tr.append(td);
+            });
+            tableBody.append(tr);
+        });
+    }
+
 });
+
+
+
